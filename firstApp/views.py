9af36a05206ee5b7110ff6fc56657fe0b715860d
fateme janesari <janesarifateme6@gmail.com>
2024-04-28 11:55:57 +0330

    first commit

---
+++ firstApp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home_page(request):
+    return render(request,'index.html')
+
+
+def about_page(request):
+    return render(request, 'about.html')
+
+
+def contact_page(request):
+    return render(request, 'contact.html')
